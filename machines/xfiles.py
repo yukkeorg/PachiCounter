3d3c37a1afad3ec2ff2e998d399b9cf7a18faca0
Yusuke Ohshima <git@yukke.org>
2012-10-28 23:28:22 +0900

    Change display design.

--- machines/xfiles.py
+++ machines/xfiles.py
@@ -10,7 +10,7 @@ COUNT_INDEX_XFILES_CHANCEGAMES = pcounter.COUNT_INDEX.USER + 2
 def init():
   return pcounter.ICounter("xfiles", to_on, to_off, output)
 
-  
+
 def to_on(cbittype, bitgroup, counts, history):
   if cbittype == pcounter.USBIO_BIT.COUNT:
     counts[pcounter.COUNT_INDEX.COUNT] += 1
@@ -38,33 +38,32 @@ def to_off(cbittype, bitgroup, counts, history):
     counts[pcounter.COUNT_INDEX.COUNT] = 0
   if cbittype == pcounter.USBIO_BIT.CHANCE:
     counts[pcounter.COUNT_INDEX.CHAIN] = 0
-  
+
 def output(counts, history):
   data_table = {
-     'count':       util.decolate_number(counts[pcounter.COUNT_INDEX.COUNT], 4), 
-     'totalcount':  util.decolate_number(counts[pcounter.COUNT_INDEX.TOTALCOUNT], 5), 
+     'count':       util.decolate_number(counts[pcounter.COUNT_INDEX.COUNT], 3),
+     'totalcount':  util.decolate_number(counts[pcounter.COUNT_INDEX.TOTALCOUNT], 4),
      'bonuscount':  util.decolate_number(counts[pcounter.COUNT_INDEX.SBONUS], 3),
-     'uzcount':     util.decolate_number(counts[pcounter.COUNT_INDEX.CHANCE], 2), 
+     'uzcount':     util.decolate_number(counts[pcounter.COUNT_INDEX.CHANCE], 2),
      'reserved':    util.decolate_number(counts[pcounter.COUNT_INDEX.BONUS], 3),
      'xrcount':     util.decolate_number(counts[COUNT_INDEX_XFILES_XR], 2),
-     'normalcount': util.decolate_number(counts[COUNT_INDEX_XFILES_NORMALGAMES], 4),
-     'uzxrcount':   util.decolate_number(counts[COUNT_INDEX_XFILES_CHANCEGAMES], 4),
-     'xr_rate':     util.gen_bonusrate(counts[COUNT_INDEX_XFILES_NORMALGAMES], 
-                                       counts[COUNT_INDEX_XFILES_XR]), 
-     'uz_rate':     util.gen_bonusrate(counts[COUNT_INDEX_XFILES_NORMALGAMES], 
-                                       counts[pcounter.COUNT_INDEX.CHANCE]), 
-     'bonus_rate':  util.gen_bonusrate(counts[pcounter.COUNT_INDEX.TOTALCOUNT], 
+     'normalcount': counts[COUNT_INDEX_XFILES_NORMALGAMES],
+     'uzxrcount':   counts[COUNT_INDEX_XFILES_CHANCEGAMES],
+     'xr_rate':     util.gen_bonusrate(counts[COUNT_INDEX_XFILES_NORMALGAMES],
+                                       counts[COUNT_INDEX_XFILES_XR]),
+     'uz_rate':     util.gen_bonusrate(counts[COUNT_INDEX_XFILES_NORMALGAMES],
+                                       counts[pcounter.COUNT_INDEX.CHANCE]),
+     'bonus_rate':  util.gen_bonusrate(counts[pcounter.COUNT_INDEX.TOTALCOUNT],
                                        counts[pcounter.COUNT_INDEX.SBONUS]),
      'xr_chain':    util.gen_chain(counts[pcounter.COUNT_INDEX.CHAIN] - 1),
      'history':     util.gen_history(history, 3, sep="\n", isfill=True),
   }
 
-  fmt = ('<span font-desc="Ricty Bold 14">' 
-         '<span size="small"><u>Now</u></span>\n' 
+  fmt = ('<span size="small"><u>Start</u></span>\n'
          '<span size="x-large">{count}</span>\n')
   if counts[pcounter.COUNT_INDEX.CHAIN] > 1:
     add_fmt = ('<span size="small"><u>XTRA-RUSH</u></span>{xr_chain}\n'
-               '<span size="small"><u>TotalBonus</u>\n{bonuscount}({bonus_rate})</span>\n')
+               '<span size="small"><u>TotalBonus</u>\n{totalcount}({bonus_rate})</span>\n')
 
   else:
     add_fmt = ('<span size="small"><u>Total</u></span>\n'
@@ -76,7 +75,7 @@ def output(counts, history):
                'XR:<span size="large">{xrcount}</span><span size="small">({xr_rate})</span>\n'
                '<span size="small"><u>History</u></span>\n'
                '{history}')
-  fmt = fmt + add_fmt + '</span>'
+  fmt = fmt + add_fmt
 
   return fmt.format(**data_table)
-  
+
