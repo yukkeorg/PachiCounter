12bfaf9c27dba9535f37c63d5ae64f7fddef6aa7
Yusuke Ohshima <git@yukke.org>
2013-06-27 23:57:05 +0900

    machines/vb.py: refactoring.

--- machines/vb.py
+++ machines/vb.py
@@ -64,18 +64,11 @@ def output(counts, history):
 
 
   if counts[COUNT_INDEX.CHAIN] > 0:
-    less100 = counts[COUNT_INDEX.CHANCE] % 100
-    less10 = less100 % 10
-    if less10 in (1, 2, 3) and not (10 <= less100 < 20):
-      t = '{0}{1}'.format(counts[COUNT_INDEX.CHANCE], ('st', 'nd', 'rd')[less10 - 1])
-    else:
-      t = '{0}th'.format(counts[COUNT_INDEX.CHANCE])
-
     continue_possibilty = ((1.0 - falldown_possibility)
                                ** counts[COUNT_INDEX_CHANCEGAMES]) * 100
     data_table.update({
        'continuepossibility' : '{0:.2f}%'.format(continue_possibilty),
-       'vat'                 : t,
+       'vat'                 : _ordinal(counts[COUNT_INDEX.CHANCE]),
     })
 
     fmt = ('<span color="#ffff00">'
@@ -95,3 +88,11 @@ def output(counts, history):
 
   return fmt.format(**data_table)
 
+
+ORDER = {1:'st', 2:'nd', 3:'rd'}
+def _ordinal(o):
+  if 10 <= (o % 100) < 20:
+    t = str(o) + 'th'
+  else:
+    t = str(o) + ORDER.get((o % 10), 'th')
+  
