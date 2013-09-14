441233b35dd644bdf9bacc1960421a239f04fd5d
Yusuke Ohshima <git@yukke.org>
2013-09-14 23:42:56 +0900

    machines/gn_json.py: update

--- machines/gn_json.py
+++ machines/gn_json.py
@@ -6,6 +6,8 @@ from pcounter.plugin import ICounter, UtilsMixin
 from pcounter.util import gen_bonusrate, bit_is_enable
 
 class gn_json(ICounter, UtilsMixin):
+  MAX_CHANCE_TIME = 100
+
   def createCountData(self):
     return CountData(('count', 'totalcount', 'bonus', 
                     'chance', 'chain', 'chancetime', 'isbonus',
@@ -14,11 +16,12 @@ class gn_json(ICounter, UtilsMixin):
   def on(self, cbtype, iostatus, cd):
     if cbtype == USBIO_BIT.COUNT:
       cd['count'] += 1
-      cd['totalcount'] += 1
+      if not bit_is_enable(iostatus, USBIO_BIT.CHANCE):
+        cd['totalcount'] += 1
     elif cbtype == USBIO_BIT.BONUS:
       cd['bonus'] += 1
       cd['isbonus'] = 1
-      if bit_is_enable(iostatus, USBIO_BIT.CHANCE):   # チャンス中
+      if bit_is_enable(iostatus, USBIO_BIT.CHANCE):
         cd['chain'] += 1
     elif cbtype == USBIO_BIT.CHANCE:
       cd['chance'] += 1
@@ -30,32 +33,33 @@ class gn_json(ICounter, UtilsMixin):
     if cbtype == USBIO_BIT.BONUS:
       cd['isbonus'] = 0
       cd['count'] = 0
-      if bit_is_enable(iostatus, USBIO_BIT.CHANCE):   # チャンス中
+      if bit_is_enable(iostatus, USBIO_BIT.CHANCE):
         cd['chancetime'] = 1
     elif cbtype == USBIO_BIT.CHANCE:
       cd['chain'] = 0
       cd['chancetime'] = 0
+      if cd['count'] >= self.MAX_CHANCE_TIME:
+        cd['totalcount'] += cd['count']
 
 
   def build(self, cd):
     d = cd.counts
     bonusrate = gen_bonusrate(d['totalcount'], d['chance'])
     if cd['chancetime'] == 1: 
-      chainstr = "CHANCE TIME - {0} Bonus".format(self.ordering(d['chain']))
       color = self.rgb2int(0xff, 0xff, 0x33)
       dd = {
         'framesvg': 'resource/orangeflame_wide.svg',
         '8'  : { 'text': '{count}'.format(**d)},
-        '9'  : { 'text': ' ',},
+        '9'  : { 'text': '<big>{chain}</big> chain'.format(**d),},
         '10' : { 'text': '{bonus} / {chance}'.format(**d)},
-        '11' : { 'text': chainstr}
+        '11' : { 'text': 'CHANCE TIME'}
       }
       self.bulk_set_color(dd, color)
       dd['8']['color'] = self.rgb2int(0, 0, 0)
     else:
       dd = {
         'framesvg': 'resource/blueflame_wide.svg',
-        '8'  : { 'text': '{count} / {totalcount}'.format(**d) },
+        '8'  : { 'text': '{count} / <small>{totalcount}</small>'.format(**d) },
         '9'  : { 'text': bonusrate },
         '10' : { 'text': '{bonus} / {chance}'.format(**d) },
         '11' : { 'text': ' ' }
