5990f534fc56791bda310ed108940d5fbe31e45d
Yusuke Ohshima <git@yukke.org>
2014-02-01 13:09:34 +0900

    uforush.py: 改善

--- machines/uforush.py
+++ machines/uforush.py
@@ -7,6 +7,7 @@ from pcounter.core import USBIO_BIT, CountData, json
 from pcounter.plugin import ICounter, UtilsMixin
 from pcounter.util import gen_bonusrate, bit_is_enable
 
+
 class DeltaTime(object):
   def __init__(self):
     self.__prev = time.time()
@@ -22,32 +23,35 @@ class DeltaTime(object):
 
 
 class BonusRoundInfo(object):
-  def __init__(self, _round, _maxsec, _bonuspt):
-    self.round = _round
-    self.maxsec = _maxsec
-    self.bonuspt = _bonuspt
+  def __init__(self, nround, limitsec, gainpts):
+    self.nround = nround
+    self.limitsec = limitsec
+    self.gainpts = gainpts
 
-def calcLossPerSecNorm(bc, r):
+
+def calcLpsOnNorm(bc, r):
   return 1.6667 * (-1.0 + ((bc * r) / (250.0 + bc * r)))
 
-def calcLossPerSecChance(base):
+def calcLpsOnChance(base):
   return 1.6667 * (-1.0 + base)
 
 
 class uforush(ICounter, UtilsMixin):
-  LosePerSec = (calcLossPerSecNorm(3, 20),  # 打出し1.6個/s 250玉で60個賞球があるときの
-                calcLossPerSecChance(0.90))
+  LPS = (calcLpsOnNorm(3, 20),     # losepts/sec
+         calcLpsOnChance(0.90))
+
+  MaxSPC = 40.0   # sec/count
 
   BonusRound = (
-    BonusRoundInfo( 1,   55.0,  80),
-    BonusRoundInfo( 2,   65.0, 160),
-    BonusRoundInfo( 3,   76.0, 240),
-    BonusRoundInfo( 4,   88.0, 320),
-    BonusRoundInfo( 5,  100.0, 400),
-    BonusRoundInfo( 6,  110.0, 480),
-    BonusRoundInfo( 7,  120.0, 560),
-    BonusRoundInfo( 8,  136.0, 640),
-    BonusRoundInfo(12, 9999.0, 960),
+    BonusRoundInfo(nround= 1, limitsec=  55.0, gainpts= 80),
+    BonusRoundInfo(nround= 2, limitsec=  65.0, gainpts=160),
+    BonusRoundInfo(nround= 3, limitsec=  76.0, gainpts=240),
+    BonusRoundInfo(nround= 4, limitsec=  88.0, gainpts=320),
+    BonusRoundInfo(nround= 5, limitsec= 100.0, gainpts=400),
+    BonusRoundInfo(nround= 6, limitsec= 110.0, gainpts=480),
+    BonusRoundInfo(nround= 7, limitsec= 120.0, gainpts=560),
+    BonusRoundInfo(nround= 8, limitsec= 136.0, gainpts=640),
+    BonusRoundInfo(nround=12, limitsec=9999.0, gainpts=960),
   )
 
   def __init__(self):
@@ -55,31 +59,34 @@ class uforush(ICounter, UtilsMixin):
     self.bonustime = DeltaTime()
 
   def createCountData(self):
-    return CountData(('count', 'totalcount', 'bonus', 
-                    'chance', 'chain', 'chancetime', 'isbonus',
-                    'sbonus', 'spg', 'spb', 'pbr', 'voutput'))
+    return CountData(('count', 'totalcount', 'bonus',
+                      'chance', 'chain', 'chancetime',
+                      'isbonus', 'sbonus', 'spg', 'spb',
+                      'pbr', 'voutput'))
 
   def detectBonus(self, t):
     for bi in self.BonusRound:
-      if t < bi.maxsec:
+      if t < bi.limitsec:
         return bi
 
   def on(self, cbtype, iostatus, cd):
+    ischance = bit_is_enable(iostatus, USBIO_BIT.CHANCE)
+
     if cbtype == USBIO_BIT.COUNT:
       cd['count'] += 1
-      ischance = bit_is_enable(iostatus, USBIO_BIT.CHANCE)
       if not ischance:
         cd['totalcount'] += 1
 
       d = self.gcdelta.getDelta()
       cd['spg'] = d
-      cd['voutput'] += (min(40.0, d) * self.LosePerSec[1 if ischance else 0])
+      cd['voutput'] += (min(self.MaxSPC, d)
+                         * self.LPS[int(ischance)])
 
     elif cbtype == USBIO_BIT.BONUS:
       self.bonustime.check()
       cd['bonus'] += 1
       cd['isbonus'] = 1
-      if bit_is_enable(iostatus, USBIO_BIT.CHANCE):
+      if ischance:
         cd['chain'] += 1
 
     elif cbtype == USBIO_BIT.CHANCE:
@@ -89,22 +96,24 @@ class uforush(ICounter, UtilsMixin):
 
 
   def off(self, cbtype, iostatus, cd):
+    ischance = bit_is_enable(iostatus, USBIO_BIT.CHANCE)
     if cbtype == USBIO_BIT.BONUS:
       cd['isbonus'] = 0
       cd['count'] = 0
-      if bit_is_enable(iostatus, USBIO_BIT.CHANCE):
+      if ischance:
         cd['chancetime'] = 1
+
       self.gcdelta.check()
       d = self.bonustime.getDelta()
       bi = self.detectBonus(d)
       cd['spb'] = d
-      cd['voutput'] += bi.bonuspt
-      cd['pbr'] = bi.round
+      cd['pbr'] = bi.nround
+      cd['voutput'] += bi.gainpts
 
     elif cbtype == USBIO_BIT.CHANCE:
       cd['chain'] = 0
       cd['chancetime'] = 0
-      cd['totalcount'] += cd['count']
+      # cd['totalcount'] += cd['count']
 
 
   def build(self, cd):
@@ -117,7 +126,7 @@ class uforush(ICounter, UtilsMixin):
         '0'  : { 'text': '{count}<small></small>'.format(**d)},
         '1'  : { 'text': '{chain}<small> CHAIN</small>'.format(**d)},
         '2'  : { 'text': '{bonus}<small> ({chance})</small>'.format(**d) },
-        '3'  : { 'text': '{voutput:.0f}<small><small>PTS</small></small>'.format(**d)},
+        '3'  : { 'text': '{voutput:.0f}<small><small> PTS</small></small>'.format(**d)},
         '4'  : { 'text': 'SPG:{spg:.2f} SPB:{spb:.2f} PBR:{pbr}r'.format(**d)},
       }
       self.bulk_set_color(dd, color)
@@ -128,7 +137,7 @@ class uforush(ICounter, UtilsMixin):
         '0' : { 'text': '{count}<small> / {totalcount}</small>'.format(**d) },
         '1' : { 'text': bonusrate },
         '2' : { 'text': '{bonus}<small> ({chance})</small>'.format(**d) },
-        '3' : { 'text': '{voutput:.0f}<small><small>PTS</small></small>'.format(**d)},
+        '3' : { 'text': '{voutput:.0f}<small><small> PTS</small></small>'.format(**d)},
         '4' : { 'text': 'SPG:{spg:.2f} SPB:{spb:.2f} PBR:{pbr}r'.format(**d)},
       }
       if cd['isbonus'] == 1:
