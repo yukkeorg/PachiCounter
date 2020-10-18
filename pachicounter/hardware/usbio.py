7f2244752ffda7b3411af1bdea046246ab245585
Yusuke Ohshima <ohshima.yusuke@yukke.org>
2020-10-18 21:08:40 +0900

    remove coding statement from first line of each file.

--- pachicounter/hardware/usbio.py
+++ pachicounter/hardware/usbio.py
@@ -1,7 +1,7 @@
-# coding: utf-8
 # vim: ts=4 sts=4 sw=4 et
 
 import pyusbio
+
 from . import HwReceiver, HwReceiverError
 
 
--- pachicounter/machine/ghostneo.py
+++ pachicounter/machine/ghostneo.py
@@ -1,4 +1,3 @@
-# coding: utf-8
 # vim: ts=4 sts=4 sw=4 et
 
 from pachicounter.core import USBIO_BIT, CountData, json
